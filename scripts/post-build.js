7c85eef53528b2cb8ef735ce76f53f02302e2932
gosho-kazuya <ketsume0211@gmail.com>
2020-04-10 03:31:03 +0900

    fix postbuild script

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 const manifestJson = fs.readFileSync(path.resolve(__dirname, '../manifest.json'), 'utf8')
 const manifest = JSON.parse(manifestJson)
-manifest.content_scripts[0].js = './index.js'
+manifest.content_scripts[0].js = ['./index.js']
 
 fs.writeFileSync(
   path.resolve(__dirname, '../build/manifest.json'),
